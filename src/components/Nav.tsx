fed34cf071c302f1eda686c2b62d6fd7bd56fb36
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-04-16 21:26:05 -0500

    NavBar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,55 @@
+const Nav = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-end"></div>
+      </div>
+      <div className=" w-full h-20 flex flex-col justify-center items-center my-20 ">
+        <p className="text-5xl font-bold text-gray-400">Hi, I'm Adrian Dav</p>
+        <p className="text-4xl font-bold text-gray-600 my-5">
+          Front-end Developer
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
