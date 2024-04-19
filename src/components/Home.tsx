581b81a47966061623acb635ad15a599dfe6af2a
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-04-19 18:17:08 -0500

    Home

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className=" w-full h-20 flex flex-col justify-center items-center my-20 ">
+      <p className="text-5xl font-bold text-gray-400">Hi, I'm Adrian Dav</p>
+      <p className="text-4xl font-bold text-gray-600 my-5">
+        Front-end Developer
+      </p>
+    </div>
+  );
+};
+
+export default Home;
