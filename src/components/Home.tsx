f8b6f377a8240b1b94a1e32ebb8c169c13361263
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-04-21 14:17:19 -0500

    dark-mode-icon

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="">
       <div className=" w-full h-20 flex flex-col justify-center items-center my-20 font-Onest">
         <p className="text-5xl font-bold text-gray-400">Hi, I'm Adrian</p>
-        <p className="text-4xl font-bold text-gray-600 my-5">
+        <p className="text-4xl font-bold text-gray-600 my-5 hover:brightness-110">
           Front-end Developer
         </p>
       </div>
