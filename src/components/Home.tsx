78c2beface7b3580be68f458758717ae87da2a44
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-04-21 09:12:17 -0500

    icons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,11 @@
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <div className=" w-full h-20 flex flex-col justify-center items-center my-20 font-Onest">
         <p className="text-5xl font-bold text-gray-400">Hi, I'm Adrian</p>
         <p className="text-4xl font-bold text-gray-600 my-5">
           Front-end Developer
         </p>
-      </div>
-      <div className="h-96 carousel carousel-vertical rounded-box  flex justify-center items-center">
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" />
-        </div>
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" />
-        </div>
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" />
-        </div>
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" />
-        </div>
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" />
-        </div>
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" />
-        </div>
-        <div className="carousel-item h-full">
-          <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" />
-        </div>
       </div>
     </div>
   );
