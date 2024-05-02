d382a2c1c34e12e4c5b81a0b88832e0cb9efda7d
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-05-02 15:01:33 -0500

    about dark

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,10 @@
 const About: React.FC = () => {
   return (
-    <div className="h-52 w-[700px] bg-zinc-400 border-2 border-zinc-400 dark:bg-zinc-900 p-5 rounded-md">
-      <h1 className="font-Onest font-bold text-2xl mb-2 text-zinc-900 ">
+    <div className="h-52 w-[700px] bg-zinc-400 border-[1px] border-zinc-200 dark:border-zinc-700  dark:bg-zinc-900 p-5 rounded-md">
+      <h1 className="font-Onest font-bold text-2xl mb-2 text-zinc-800 dark:text-zinc-300 ">
         About Me
       </h1>
-      <p className="font-Onest text-zinc-900  font-semibold text-lg">
+      <p className="font-Onest text-zinc-700 font-semibold text-lg dark:text-zinc-400">
         I am a young man passionate about web development. Although I haven't
         been around for a long time, I want to improve my skills and my logic
         more and more every day, specializing in back-end and new emerging
