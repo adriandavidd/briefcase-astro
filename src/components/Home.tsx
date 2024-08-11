452c57073f0e309489e23eb5d0691128db7f9865
Adrian Uriel <adrianurielvergaraa@gmail.com>
2024-08-11 06:50:20 -0700

    carousel-delete

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,10 @@
 import TextAnimation from "./TextAnimation";
 import About from "./About";
-import Carousel from "../components/Carousel";
 const Home = () => {
   return (
     <div className="">
       <TextAnimation></TextAnimation>
-      <div className="flex items-center justify-center">
-        <Carousel></Carousel>
-      </div>
+      <div className="flex items-center justify-center">s</div>
       <div className="mt-11 w-full flex justify-center items-center bg-zinc-300  dark:bg-[#0a0a0a]">
         <About></About>
       </div>
